82f6bcc8e6ca4db7717868402c236c16a799d333
FeliGC <felipitogc@hotmail.com>
2022-06-30 22:41:37 -0300

    controllers and routers

--- Sprint2/Vinoteca/app.js
+++ Sprint2/Vinoteca/app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.set("view engine", "ejs")
+
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
+
+
+const mainRouter= require("./routers/mainRouter")
+
+
+
+
+
 
 app.get('/', (req, res) => {
 res.sendFile(path.resolve(__dirname, 'views/home.html'))
