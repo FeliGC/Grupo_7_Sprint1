bf41dcf44140e1c45578059fe5c20121890a6749
FeliGC <felipitogc@hotmail.com>
2022-07-14 15:59:59 -0300

    Sprint4

---
+++ Sprint4/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+const methodOverride = require("method-override")
+app.use(methodOverride ("_method"))
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+const mainRoutes = require ('./routers/mainRoutes');
+const productsRoutes = require ('./routers/productsRoutes');
+const usersRoutes = require ('./routers/usersRoutes');
+app.use('/', mainRoutes);
+app.use('/products', productsRoutes);
+app.use('/users', usersRoutes);
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo');
+});
