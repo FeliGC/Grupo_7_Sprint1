ba73d3ed8eaa810c746bc4b5c6069e6209113888
FeliGC <felipitogc@hotmail.com>
2022-10-13 20:23:29 -0300

    proyecto final sin pass

--- Sprint8/src/database/config/config.js
+++ Sprint8/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "1212",
+    "password": "",
     "database": "vinoteca_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
